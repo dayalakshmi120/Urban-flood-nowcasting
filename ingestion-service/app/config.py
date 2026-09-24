98a17e8dbaa9ed631e3e520b35fc833767673120
dayalakshmi120 <dayalakshmi120@gmail.com>
2026-09-24 19:57:56 +0530

    Add ingestion service configuration
    Added configuration for MQTT and Kafka settings.

---
+++ ingestion-service/app/config.py
@@ -0,0 +1,16 @@
+import os
+
+
+MQTT_BROKER = os.getenv("MQTT_BROKER", "localhost")
+MQTT_PORT = int(os.getenv("MQTT_PORT", "1883"))
+MQTT_TOPIC = os.getenv("MQTT_TOPIC", "flood/sensors")
+
+KAFKA_BOOTSTRAP_SERVERS = os.getenv(
+    "KAFKA_BOOTSTRAP_SERVERS",
+    "localhost:9092"
+)
+
+KAFKA_TOPIC = os.getenv(
+    "KAFKA_TOPIC",
+    "water-sensor-events"
+)
